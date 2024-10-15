57d4727546a02d9fd2d721b6c469d72dcf513957
Om Bharsakle <ombharsakle418@gmail.com>
2024-10-15 23:53:39 +0530

    HI

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -15,6 +15,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
+
     double height = MediaQuery.of(context).size.height;
     SearchProvider searchProviderFalse =
     Provider.of<SearchProvider>(context, listen: false);
